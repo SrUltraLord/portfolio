b5dfac78b1265ec73db13acce619a7fda62dcdd4
SrUltraLord <davidrey8822@gmail.com>
2023-04-04 21:41:49 -0500

    feat: tailwind and svelte plugins.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "surface-primary": "#FFFFFF",
+        "surface-secondary": "#494D64",
+        "accent-primary": "#FF0000",
+        "foreground-primary": "#F7F7F9",
+      },
+      fontFamily: {
+        montserrat: "Montserrat",
+        iosevka: "Iosevka",
+      },
+    },
   },
   plugins: [],
 };
