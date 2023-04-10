061f37f87f01f79772f298f53ad4fab50a0e5798
SrUltraLord <davidrey8822@gmail.com>
2023-04-10 16:34:13 -0500

    fix: navbar responsiveness for md-sized devices.

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -8,6 +8,7 @@
   };
 
   const links: Link[] = [
+    { title: "Home", to: "landing" },
     { title: "About me", to: "about" },
     { title: "Experience", to: "experience" },
     { title: "Projects", to: "projects" },
@@ -18,6 +19,8 @@
 
   const handleToggleMenu = () => (isOpen = !isOpen);
 
+  const closeMenu = () => (isOpen = false);
+
   const handleDownloadResume = () => {
     console.log("Downloading epic resume");
   };
@@ -26,9 +29,11 @@
 <nav class="fixed w-full overflow-hidden shadow-md">
   <!-- Desktop Menu -->
   <div
-    class="flex justify-between bg-slate-100 bg-white/30 px-2 py-4 backdrop-blur-xl"
+    class="z-50 flex justify-between bg-slate-100 bg-white/30 p-6 backdrop-blur-xl"
   >
-    <img class="h-6 w-6" src="./favicon.svg" alt="astro-icon" />
+    <a on:click={closeMenu} href="#landing">
+      <img class="h-6 w-6" src="./favicon.svg" alt="astro-icon" />
+    </a>
 
     <button
       class="block transition-transform md:hidden"
@@ -42,9 +47,11 @@
     </button>
 
     <!-- Desktop Right Content -->
-    <div class="hidden gap-8 md:flex text-sm md:text-base">
+    <div class="hidden gap-8 text-sm md:flex md:text-base">
       <!-- Links container -->
-      <div class="flex flex-row items-center gap-6 text-right">
+      <div
+        class="flex flex-row items-center gap-6 text-right text-foreground-primary"
+      >
         {#each links as { title, to }}
           <a
             href={`#${to}`}
@@ -69,16 +76,21 @@
   <!-- Mobile Right Content -->
   {#if isOpen}
     <div
-      class="flex h-screen justify-end bg-white bg-white/30 shadow-md backdrop-blur-xl transition duration-300 ease-in-out"
+      class="flex h-screen justify-end bg-white bg-white/30 shadow-md backdrop-blur-xl transition duration-300 ease-in-out md:hidden"
     >
       <!-- Links and Button Container -->
-      <div class="h-screen w-2/3 bg-slate-100 pr-4 text-right sm:hidden flex flex-col gap-8">
+      <div
+        class="flex h-screen w-2/3 flex-col gap-8 bg-slate-100 pr-4 text-right md:hidden"
+      >
         <!-- Links -->
-        <div class="static right-0 top-0 z-10 mt-16 flex flex-col gap-4">
+        <div
+          class="static right-0 top-0 z-10 mt-16 flex flex-col gap-4 text-foreground-primary"
+        >
           {#each links as { title, to }}
             <a
               href={`#${to}`}
-              class="text-md font-iosevka font-light transition hover:font-bold hover:text-accent-primary hover:underline sm:text-sm"
+              class="text-md font-iosevka font-light transition hover:font-bold hover:text-accent-primary hover:underline md:text-xl"
+              on:click={closeMenu}
             >
               {title}
             </a>
